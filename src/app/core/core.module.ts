632971450a76d116cd1f7fe7d40abe353466b89b
emmemorandini <emmemorandini@gmail.com>
2023-04-25 20:33:40 +0100

    Setup test environment

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ShellComponent } from './shell/shell.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ShellComponent} from './shell/shell.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './footer/footer.component';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
-
 
 
 @NgModule({
@@ -25,4 +24,5 @@ import {SharedModule} from '../shared/shared.module';
     ShellComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
