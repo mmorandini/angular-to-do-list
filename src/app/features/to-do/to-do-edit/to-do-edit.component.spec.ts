23b9a05667efa99ef1741489fdcf71730a5540bd
emmemorandini <emmemorandini@gmail.com>
2023-04-25 20:10:05 +0100

    Adding unit tests

--- src/app/features/to-do/to-do-edit/to-do-edit.component.spec.ts
+++ src/app/features/to-do/to-do-edit/to-do-edit.component.spec.ts
@@ -1,23 +1,96 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ToDoEditComponent } from './to-do-edit.component';
+import { FormBuilder } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToDoEditComponent, EditComponentModes } from './to-do-edit.component';
+import { ToDo } from '../store/to-do.state';
 
 describe('ToDoEditComponent', () => {
   let component: ToDoEditComponent;
   let fixture: ComponentFixture<ToDoEditComponent>;
+  let formBuilder: FormBuilder;
+  let activeModal: NgbActiveModal;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToDoEditComponent ]
+      declarations: [ ToDoEditComponent ],
+      providers: [ FormBuilder, NgbActiveModal ]
     })
-    .compileComponents();
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ToDoEditComponent);
     component = fixture.componentInstance;
+    component.toDo = {
+      id: 1,
+      label: 'Test To Do',
+      description: 'This is a test to do.',
+      category: 'Test Category',
+      done: false,
+    };
+    formBuilder = TestBed.inject(FormBuilder);
+    activeModal = TestBed.inject(NgbActiveModal);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form group', () => {
+    expect(component.formGroup).toBeDefined();
+  });
+
+  it('should have valid initial form values when in create mode', () => {
+    component.mode = EditComponentModes.Create;
+    component.toDo = undefined;
+    component.ngOnInit();
+    expect(component.controls['label'].value).toBe('');
+    expect(component.controls['description'].value).toBe('');
+    expect(component.controls['category'].value).toBe('');
+  });
+
+  it('should have valid initial form values when in update mode', () => {
+    const todo: ToDo = {
+      id: 1,
+      label: 'Test Label',
+      description: 'Test Description',
+      category: 'Test Category',
+      done: false
+    };
+    component.mode = EditComponentModes.Update;
+    component.toDo = todo;
+    component.ngOnInit();
+    expect(component.controls['label'].value).toBe(todo.label);
+    expect(component.controls['description'].value).toBe(todo.description);
+    expect(component.controls['category'].value).toBe(todo.category);
+  });
+
+  it('should cancel the edit and dismiss the modal', () => {
+    spyOn(activeModal, 'dismiss');
+    component.cancel();
+    expect(activeModal.dismiss).toHaveBeenCalled();
+  });
+
+  it('should submit the form and close the modal if the form is valid', () => {
+    spyOn(activeModal, 'close');
+    const todo: ToDo = {
+      id: 1,
+      label: 'Test Label',
+      description: 'Test Description',
+      category: 'Test Category',
+      done: false
+    };
+    component.toDo = todo;
+    component.ngOnInit();
+    component.submit();
+    expect(activeModal.close).toHaveBeenCalledWith({payload: {...todo, ...component.formGroup.value}});
+  });
+
+  it('should mark all form fields as touched if the form is invalid', () => {
+    component.formGroup.setErrors({invalid: true});
+    spyOn(component.formGroup, 'markAllAsTouched');
+    component.submit();
+    expect(component.formGroup.markAllAsTouched).toHaveBeenCalled();
   });
 });
--- src/app/features/to-do/to-do.service.spec.ts
+++ src/app/features/to-do/to-do.service.spec.ts
@@ -1,16 +1,89 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ToDoService } from './to-do.service';
+import { ToDo } from './store/to-do.state';
+import { environment } from '../../../environments/environment';
 
 describe('ToDoService', () => {
+  let httpMock: HttpTestingController;
   let service: ToDoService;
+  const apiUrl = environment.apiUrl + '/tasks';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ToDoService]
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ToDoService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getToDos', () => {
+    it('should return an Observable<ToDo[]>', () => {
+      const dummyToDos: ToDo[] = [
+        { id: 1, description: 'Buy groceries', label: 'my-label', category: 'my-category', done: false },
+        { id: 2, description: 'Buy groceries', label: 'my-label', category: 'my-category', done: false }
+      ];
+
+      service.getToDos().subscribe((toDos) => {
+        expect(toDos.length).toBe(2);
+        expect(toDos).toEqual(dummyToDos);
+      });
+
+      const req = httpMock.expectOne(apiUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyToDos);
+    });
+  });
+
+  describe('addToDo', () => {
+    it('should return an Observable<ToDo>', () => {
+      const dummyToDo: ToDo = { id: 1, description: 'Buy groceries', label: 'my-label', category: 'my-category', done: false };
+
+      service.addToDo(dummyToDo).subscribe((toDo) => {
+        expect(toDo).toEqual(dummyToDo);
+      });
+
+      const req = httpMock.expectOne(apiUrl);
+      expect(req.request.method).toBe('POST');
+      req.flush(dummyToDo);
+    });
+  });
+
+  describe('updateToDo', () => {
+    it('should return an Observable<ToDo>', () => {
+      const dummyToDo: ToDo = { id: 1, description: 'Buy groceries', label: 'my-label', category: 'my-category', done: false };
+
+      service.updateToDo(dummyToDo).subscribe((toDo) => {
+        expect(toDo).toEqual(dummyToDo);
+      });
+
+      const req = httpMock.expectOne(`${apiUrl}/${dummyToDo.id}`);
+      expect(req.request.method).toBe('PATCH');
+      req.flush(dummyToDo);
+    });
+  });
+
+  describe('deleteToDo', () => {
+    it('should return an Observable<void>', () => {
+      const dummyId = 1;
+
+      service.deleteToDo(dummyId).subscribe(() => {
+        expect().nothing();
+      });
+
+      const req = httpMock.expectOne(`${apiUrl}/${dummyId}`);
+      expect(req.request.method).toBe('DELETE');
+      req.flush({});
+    });
   });
 });
