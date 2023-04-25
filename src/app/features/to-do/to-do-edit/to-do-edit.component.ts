ab774bde6888dfc3813b96efe5805e9bc3d04363
emmemorandini <emmemorandini@gmail.com>
2023-04-25 20:07:15 +0100

    added to-do-edit.component markup and logic

--- src/app/features/to-do/to-do-edit/to-do-edit.component.ts
+++ src/app/features/to-do/to-do-edit/to-do-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ToDo} from '../store/to-do.state';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 export enum EditComponentModes {
   Create = 0,
@@ -16,6 +19,43 @@ interface ToDoForm {
   templateUrl: './to-do-edit.component.html',
   styleUrls: ['./to-do-edit.component.scss']
 })
-export class ToDoEditComponent {
+export class ToDoEditComponent implements OnInit {
+  @Input() toDo: ToDo | undefined;
+  @Input() mode: EditComponentModes | undefined;
+  formGroup: FormGroup;
+
+  get controls() {
+    return this.formGroup.controls;
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private activeModal: NgbActiveModal
+  ) {
+    this.formGroup = this.initForm();
+  }
+
+  ngOnInit(): void {}
+
+  initForm(): FormGroup {
+    return this.formBuilder.group({
+      label: [this.toDo ? this.toDo.label : '', [Validators.required]],
+      description: [this.toDo ? this.toDo.description : '', [Validators.required, Validators.maxLength(300)]],
+      category: [this.toDo ? this.toDo.category : '']
+    });
+  }
+
+  cancel(): void {
+    this.activeModal.dismiss();
+  }
+
+  submit(): void {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+    const formData: ToDoForm = this.formGroup.value;
+    this.activeModal.close({payload: {...this.toDo, ...formData, done: false}});
+  }
 
 }
